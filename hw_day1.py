009008d61790380181077e3a19894bbe55b30b56
unknown <nadia.h.hawa@gmail.com>
2022-05-02 22:56:46 -0400

    revised commit, confused about directions

--- hw_day1.py
+++ hw_day1.py
@@ -7,7 +7,26 @@ for line in lines:
     # print(line)
     match = re.search(r'([A-Z][a-z]+\b).+([A-Z][a-z]+\b)', line)
     if (match != None):
-        print(match.group(2))
-        # print(match.groups())
-        
+        print(match.group(1),match.group(2))
+        # print(match.group(2))
+        #*** Was a little confused on the rubric on what exactly the output should be since the directions stated only last name
+        #That solution is listed above but commented out. 
+        #Use python to read the file regex_test.txt and print the 
+        #last name on each line using regular expressions and groups (return None for names with no first and last name, or names that aren't properly capitalized)
+        #But the expected output is as follows:
+        #"""
+        # Expected Output
+        # Abraham Lincoln
+        # Andrew P Garfield
+        # Connor Milliken
+        # Jordan Alexander Williams
+        # None
+        # None
+        # """
+
+    else:
+        print(f'{match}')
+
+
+
 #i need to create a pattern that will only take range a-z with a space a-z again and then end and print back out
